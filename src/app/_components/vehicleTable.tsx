8fc464a1d025b8198ce3b05c3139043c05ff2985
Kewan Seymour <101571402+Kweezy01@users.noreply.github.com>
2024-08-06 09:09:13 +0200

    Unchecked Push, dont crash

--- src/app/_components/vehicleTable.tsx
+++ src/app/_components/vehicleTable.tsx
@@ -25,11 +25,7 @@ export function VehicleTable() {
     return (
         <div className="w-full max-w-xs">
             {latestVehicle ? (
-                <p className="truncate">Your most recent post: {latestVehicle.VIN}</p>
-            ) : (
-                <p>You have no posts yet.</p>
-            )}
-            {/* <main className="flex bg-black h-screen justify-center">
+                <main className="flex bg-black h-screen justify-center">
                 <div className="text-black bg-center bg-contain bg-[url('https://scontent-jnb1-1.xx.fbcdn.net/v/t39.30808-6/260277972_10227320945231659_2177364538079183029_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=1ac024&_nc_ohc=BNUj0U7h4X0AX9ag8BH&_nc_ht=scontent-jnb1-1.xx&oh=00_AfDC04u-kGWkfzpbF3M2J_AV6brZETHXZaWgnR4ow5Zj7A&oe=65175085')] w-full     border-x border-slate-400 md:max-w-2xl">
                     <div className="flex pl-1 pt-1 pb-1 border-b border-slate-400">
                         <nav>
@@ -40,22 +36,22 @@ export function VehicleTable() {
                     </div>
                     <table className="mt-1 w-full">
                         <tr>
-                            <td className="font-bold text-center border-b border-x bg-blue-500">Team</td>
-                            <td className="font-bold text-center border-b border-x bg-blue-500">Skipper</td>
-                            <td className="font-bold text-center border-b border-x bg-blue-500">CO-Angler</td>
-                            <td className="font-bold text-center border-b border-x bg-blue-500">Total Fish</td>
-                            <td className="font-bold text-center border-b border-x bg-blue-500">Total Weight</td>
-                            <td className="font-bold text-center border-b border-x bg-blue-500">Recorded Games</td>
+                            <td className="font-bold text-center border-b border-x bg-blue-500">StockNum</td>
+                            <td className="font-bold text-center border-b border-x bg-blue-500">VIN</td>
+                            <td className="font-bold text-center border-b border-x bg-blue-500">MMCode</td>
+                            <td className="font-bold text-center border-b border-x bg-blue-500">Odometer</td>
+                            <td className="font-bold text-center border-b border-x bg-blue-500">Standin Price</td>
+                            <td className="font-bold text-center border-b border-x bg-blue-500">Internet Price</td>
                         </tr>
                         <tr className="bg-black bg-opacity-70 text-slate-300 font-bold ml-6 border-b">
-                            {[data?.teamName, data?.angler1, data?.angler2, data?.totalFish, data?.totalWeight, data?.recordedGames].map((e) => {
+                            {[latestVehicle?.StockNum, latestVehicle?.VIN, latestVehicle?.MMcode, latestVehicle?.Odometer, latestVehicle?.StandinPrice, latestVehicle?.InternetPrice].map((e) => {
                                 return (
                                     <td key={e} className="text-center border-x">{e}</td>
                                 )
                             })}
                         </tr>
                     </table>
-
+{/* 
                     <br />
 
                     <button className="ml-1 bg-green-700 hover:bg-green-800  py-2 px-4 border border-lime-900 rounded">
@@ -86,10 +82,14 @@ export function VehicleTable() {
 
                     <button className="ml-1 bg-red-700 hover:bg-red-800  py-2 px-4 border border-lime-900 rounded">
                         <Link href={`${teamName}/delete`}>Delete</Link>
-                    </button>
+                    </button> */}
 
                 </div>
-            </main> */}
+            </main>
+            ) : (
+                <p>You have no posts yet.</p>
+            )}
+            
         </div>
     );
 }
