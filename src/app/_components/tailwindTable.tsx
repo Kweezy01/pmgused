5b7cfecbecfc1033cf2d19fdf8189b0492f8646b
Kewan Seymour <101571402+Kweezy01@users.noreply.github.com>
2024-09-03 10:11:44 +0000

    Added buttons to table

--- src/app/_components/tailwindTable.tsx
+++ src/app/_components/tailwindTable.tsx
@@ -1,66 +1,66 @@
 "use client";
 
-import { useState } from "react";
-
 import { api } from "~/trpc/react";
 
 export function TailwindTable() {
    const { data } = api.vehicle.getAll.useQuery();
 
-   // const utils = api.useUtils();
-   // const [stockNum, setStockNum] = useState("");
-   // const [VIN, setVIN] = useState("");
-   // const [MMCode, setMMCode] = useState(0);
-   // const [odometer, setOdometer] = useState(0);
-   // const [standInValue, setStandInValue] = useState(0);
-   // const [interetPrice, setInteretPrice] = useState(0);
-
-   // const createVehicle = api.vehicle.create.useMutation({
-   //     onSuccess: async () => {
-   //         await utils.vehicle.invalidate();
-   //         setStockNum("");
-   //     },
-   // });
-
    return (
       <div className="">
          {data ? (
-            <div className="flex min-h-screen items-center justify-center">
-               <div className="overflow-x-auto">
-                  <table className="min-w-full bg-white shadow-md rounded-xl">
-                     <thead>
-                        <tr className="bg-blue-gray-100 text-gray-700">
-                           <th className="py-3 px-4 text-left">Stock Number</th>
-                           <th className="py-3 px-4 text-left">Vehicle</th>
-                           <th className="py-3 px-4 text-left">VIN</th>
-                           <th className="py-3 px-4 text-left">Internet Price</th>
-                           <th className="py-3 px-4 text-left">Actions</th>
-                        </tr>
-                     </thead>
-                     <tbody className="text-blue-gray-900">
+            <>
+               <div className="flex pl-1 pt-1 pb-1 border-b border-slate-400">
+                  <nav className="w-full bg-neutral-600 relative">
+                     <button
+                        type="button"
+                        className="inline-block rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                        <a href="/" className="px-6">Home</a>
+                     </button>
+                     <button
+                        type="button"
+                        className="inline-block rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                        <a href="/insertvehicle" className="px-6">Insert Vehicle</a>
+                     </button>
+                     <button
+                        type="button"
+                        className="inline-block rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs absolute right-1 font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                        Delete Vehicle
+                     </button>
+                  </nav>
+               </div>
+               <h1 className="pl-6 pt-3 underline font-bold">Active trade Centre Vehicles</h1>
+               <div className="flex min-h-screen items-center justify-center">
+                  <div className="overflow-x-auto">
+                     <table className="min-w-full bg-white shadow-md rounded-xl">
+                        <thead>
+                           <tr className="bg-blue-gray-100 text-gray-700">
+                              <th className="py-3 px-4 text-left">Stock Number</th>
+                              <th className="py-3 px-4 text-left">Vehicle</th>
+                              <th className="py-3 px-4 text-left">VIN</th>
+                              <th className="py-3 px-4 text-left">Internet Price</th>
+                              <th className="py-3 px-4 text-left">Actions</th>
+                           </tr>
+                        </thead>
+                        <tbody className="text-blue-gray-900">
 
-                        {data.map((e) => {
-                           return (
-                              <tr className="border-b border-blue-gray-200" key={e.StockNum}>
-                                 <td className="py-3 px-4">{e.StockNum}</td>
-                                 <td className="py-3 px-4">{e.Model}</td>
-                                 <td className="py-3 px-4">{e.VIN}</td>
-                                 <td className="py-3 px-4">{e.StandInValue}</td>
-                                 <td className="py-3 px-4">
-                                    <a href="/" className="font-medium text-blue-600 hover:text-blue-800">Edit</a>
-                                 </td>
-                              </tr>
-                           )
-                        })}
-                     </tbody>
-                  </table>
-                  <div className="w-full pt-5 px-4 mb-8 mx-auto ">
-                     <div className="text-sm text-gray-700 py-1 text-center">
-                        Made with <a className="text-gray-700 font-semibold" href="https://www.material-tailwind.com/docs/html/table/?ref=tailwindcomponents" target="_blank">Material Tailwind</a> by <a href="https://www.creative-tim.com?ref=tailwindcomponents" className="text-gray-700 font-semibold" target="_blank"> Creative Tim</a>.
-                     </div>
+                           {data.map((e) => {
+                              return (
+                                 <tr className="border-b border-blue-gray-200" key={e.StockNum}>
+                                    <td className="py-3 px-4">{e.StockNum}</td>
+                                    <td className="py-3 px-4">{e.Model}</td>
+                                    <td className="py-3 px-4">{e.VIN}</td>
+                                    <td className="py-3 px-4">{e.StandInValue}</td>
+                                    <td className="py-3 px-4">
+                                       <a href="/" className="font-medium text-blue-600 hover:text-blue-800">Edit</a>
+                                    </td>
+                                 </tr>
+                              )
+                           })}
+                        </tbody>
+                     </table>
                   </div>
                </div>
-            </div>
+            </>
          ) : (
             <p>Loading database...</p>
          )}
